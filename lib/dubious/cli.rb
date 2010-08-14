a5262b375facfdc1fef7636f3e8f1b965955a21e
Nick Howard <nick@gnipcentral.com>
2010-08-13 18:53:21 -0600

    fixing up some generator action
    dubious generate controller name
    dubious generate model name

--- lib/dubious/cli.rb
+++ lib/dubious/cli.rb
@@ -1,23 +1,46 @@
 require 'thor'
 require 'thor/group'
+require 'active_support'
+
 module Dubious
   class Init < Thor::Group
     include Thor::Actions
     argument :name
-    source_root File.dirname(__FILE__)+"/templates"
+    source_root File.dirname(__FILE__)+"/templates/base"
 
     def init
       directory '.', "#{name}/"
     end
 
     def self.subcommand_help command_name
-'foo'
+      'init a new project'
+    end
+  end
+
+  class Generator < Thor
+    include Thor::Actions
+    source_root File.dirname(__FILE__)+"/templates/generator"
+
+    desc "model NAME", "creates model file" 
+    argument :name
+    def model#(name)
+      
+      template "model.mirah.tt", "app/models/#{name.underscore}.mirah"
+    end
+
+    desc "controller NAME", "creates controller file"
+    def controller#(name)
+      template "controller.mirah.tt", "app/controllers/#{name.underscore}_controller.mirah"
+      empty_directory "app/views/#{name.underscore}"
     end
   end
 
   class CLI < Thor
     desc "init NAME", "initializes a new dubious project in NAME"
     subcommand :init, Init
+
+    desc "generate GENERATOR", "run generator GENERATOR"
+    subcommand :generate, Generator
   end
 
 end
