45ba45678717afe3466dd70885c7636fd0a20659
Nick Howard <ndh@baroquebobcat.com>
2010-11-14 20:12:57 -0600

    clean up spec and do some proper namespacing etc

--- spec/dubious_cli_spec.rb
+++ spec/dubious_cli_spec.rb
@@ -1,9 +1,25 @@
 require File.dirname(__FILE__)+'/../lib/dubious/cli'
 describe Dubious::CLI do
-
+  before :each do 
+    FileUtils.rm_rf 'spec/temp'
+    Dir.mkdir 'spec/temp'
+  end
   describe "subcommands" do
-    subject { Dubious::CLI.subcommands }
+    subject { Dubious::CLI::Main.subcommands }
     it { should include 'new' }
     it { should include 'generate' }
+  end
+
+  describe Dubious::CLI::Generator do
+    it 'expects a name' do
+      lambda {
+        Dubious::CLI::Generator.new        
+      }.should raise_error
+    end
+    describe "#model" do
+      it '...' do
+        Dubious::CLI::Generator.new(['something']).tap{|g|g.destination_root=File.dirname(__FILE__)+'/temp'}.model
+      end
+    end
   end
 end
